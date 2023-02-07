2b385620590be856bc64124ba94d76316a9dcbb2
Luis Felipe Rezende <lfsrsprofessional@gmail.com>
2023-02-06 21:04:04 -0300

    Inheritance of ERC-721 is now functional

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract SimpleCollectible{
-
+contract SimpleCollectible is ERC721 {
+    // Remember the inheritance of a contract that has a constructor demands inheriting it as well.
+    constructor() public ERC721("Puppy", "PUP") {}
 }
