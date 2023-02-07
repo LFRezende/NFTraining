3617303c9d1f64fa3c98348999e3b763bea1ba48
Luis Felipe Rezende <lfsrsprofessional@gmail.com>
2023-02-07 19:41:47 -0300

    functions created

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -5,18 +5,23 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SimpleCollectible is ERC721 {
     // Remember the inheritance of a contract that has a constructor demands inheriting it as well.
-    // Visibility for Constructor is Ignored --> No need to declare public.
-    constructor() ERC721("Puppy", "PUP") {}
+    // Visibility for Constructor is Ignored --> No need to declare public (high versions from 0.7)
+    constructor() public ERC721("Puppy", "PUP") {}
+
     // tokenCounter amounts the owners of a PUP-like ERC-721 protocol.
     uint256 tokenCounter = 0;
+
     // Creating and Assigning (uniquely) multiple NFTs
     // Safe Minting garanties the tokenId is unique, not repeated
     // Minting new NFTs
-    function createCollectible(string memory tokenURI) public returns (uint256){
+    function createCollectible(string memory tokenURI)
+        public
+        returns (uint256)
+    {
         uint256 newTokenId = tokenCounter;
         _safeMint(msg.sender, newTokenId);
         _setTokenURI(newTokenId, tokenURI);
-        tokenCounter ++;
+        tokenCounter++;
         return newTokenId;
     }
 }
--- scripts/dac.py
+++ scripts/dac.py
@@ -1,5 +1,25 @@
+from scripts.utils import getAccount
+from brownie import SimpleCollectible
+
+
+TOKEN_URI = "ipfs://Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
 
 
 def deployContract():
     account = getAccount()
-    
+    tx_deploy = SimpleCollectible.deploy({"from": account})
+    tx_deploy.wait(1)
+    return tx_deploy
+
+
+def mintNFT():
+    account = getAccount()
+    ctt = SimpleCollectible[-1]
+    tx_minted = ctt.createCollectible(TOKEN_URI, {"from": account})
+    tx_minted.wait(1)
+    return tx_minted
+
+
+def main():
+    deployContract()
+    mintNFT()
