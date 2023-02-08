16e7ab8c60813e9ea90ad626132e3f30e428003a
Luis Felipe Rezende <lfsrsprofessional@gmail.com>
2023-02-08 00:56:06 -0300

    Tidying

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -6,10 +6,10 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract SimpleCollectible is ERC721 {
     // Remember the inheritance of a contract that has a constructor demands inheriting it as well.
     // Visibility for Constructor is Ignored --> No need to declare public (high versions from 0.7)
-    constructor() public ERC721("Puppy", "PUP") {}
+    constructor() public ERC721("HASHI", "HSH") {}
 
     // tokenCounter amounts the owners of a PUP-like ERC-721 protocol.
-    uint256 tokenCounter = 0;
+    uint256 public tokenCounter = 0;
 
     // Creating and Assigning (uniquely) multiple NFTs
     // Safe Minting garanties the tokenId is unique, not repeated
--- scripts/dac.py
+++ scripts/dac.py
@@ -2,13 +2,15 @@ from scripts.utils import getAccount
 from brownie import SimpleCollectible
 
 
-TOKEN_URI = "ipfs://Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+TOKEN_URI = "ipfs://QmYx6GsYAKnNzZ9A6NvEKV9nf1VaDzJrqDR23Y8YSkebLU?filename=shiba-inu.png"
+OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
 
 
 def deployContract():
     account = getAccount()
+    # REMEMBER: DEPLOYMENT DOES NOT REQUIRE WAITING!
     tx_deploy = SimpleCollectible.deploy({"from": account})
-    tx_deploy.wait(1)
+    # tx_deploy.wait(1)
     return tx_deploy
 
 
@@ -21,5 +23,9 @@ def mintNFT():
 
 
 def main():
-    deployContract()
-    mintNFT()
+    deployed = deployContract()
+    minted = mintNFT()
+    print(
+        f"You can view your NFT at {OPENSEA_URL.format(deployed.address, deployed.tokenCounter()-1)}"
+    )
+    print("You may have to wait up to 20 minutes for the minting to be successful.")
