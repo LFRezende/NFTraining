c32977f8cf0e6df97d57967c8aff14327e768c38
Luis Felipe Rezende <lfsrsprofessional@gmail.com>
2023-02-14 20:45:08 -0300

    Added events to Adv Collectible

--- contracts/AdvancedCollectible.sol
+++ contracts/AdvancedCollectible.sol
@@ -13,7 +13,7 @@ contract AdvancedCollectible is ERC721, VRFConsumerBase {
         SHIBA_INU,
         ST_BERNARD
     }
-    uint256 DogAmount = Breed.length;
+    uint256 DogAmount = 3;
 
     constructor(
         address _vrfCoordinator, // The Address for the VRFCoordinator contract in the chain
@@ -33,10 +33,13 @@ contract AdvancedCollectible is ERC721, VRFConsumerBase {
 
     mapping(uint256 => Breed) public tokenIdtoBreed;
     mapping(bytes32 => address) public requestIdtoSender;
+    event requestedCollectible(bytes32 indexed requestId, address requester);
+    event breedAssigned(uint256 indexed tokenId, Breed breed);
 
     function createCollectible() public returns (bytes32) {
-        bytes32 requestId = requestRandomness(_keyhash, _fee); // Request a random number from the VRFCoord.
+        bytes32 requestId = requestRandomness(keyhash, fee); // Request a random number from the VRFCoord.
         requestIdtoSender[requestId] = msg.sender; // !!We register the address of the sender for future use!!
+        emit requestedCollectible(requestId, msg.sender); // Good practice of programming.
     }
 
     function fulfillRandomness(bytes32 requestId, uint256 randomNumber)
@@ -46,8 +49,20 @@ contract AdvancedCollectible is ERC721, VRFConsumerBase {
         Breed breed = Breed(randomNumber % DogAmount); // Sorting out the NFT to be minted!
         uint256 newTokenId = tokenCounter; // Grabbing the tokenId for the Minting (must be unique)
         tokenIdtoBreed[newTokenId] = breed; // Identify the tokenId to your PUP
+        emit breedAssigned(newTokenId, breed); // Emit event that it has been bred! Remap --> emit event.
         address owner = requestIdtoSender[requestId]; // The one who first asked for minting!
         _safeMint(owner, newTokenId); // Mints the NFT and registers it to the owner, mapping to the newTokenId.
+        // _setTokenURI(newTokenId, TOKEN_URI)
         tokenCounter++; // Adds to the tokenCounter in order for the next mint to have a unique id as well.
+    }
+
+    // For now, we set the TokenURI
+    function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
+        // Verifies if is approved to call via ERC721 functions.
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: Caller not approved."
+        );
+        _setTokenURI(tokenId, _tokenURI);
     }
 }
