419aced738338fa94d8350597328afd6f30b42ee
Luis Felipe Rezende <lfsrsprofessional@gmail.com>
2023-02-16 02:32:28 -0300

    NFT minted

--- scripts/advanced_collectible/dac.py
+++ scripts/advanced_collectible/dac.py
@@ -1,5 +1,6 @@
-from scripts.utils import getAccount, getContract, config, network, Web3, OPENSEA_URL
+from scripts.utils import getAccount, getContract, config, network, OPENSEA_URL
 from brownie import AdvancedCollectible
+from web3 import Web3
 
 
 TOKEN_URI = "ipfs://QmSsYRx3LpDAb1GZQm7zZ1AuHZjfbPkD6J7s9r41xu1mf8?filename=pug.png"
@@ -17,15 +18,21 @@ def deployContract():
     )
     print("-->  Advanced Collectible Deployed!")
     fundLink(adv.address)
+    creating_tx = adv.createCollectible({"from": account})
+    creating_tx.wait(1)
+    print("NFT minted!")
     return adv
 
 
-def fundLink(contract_address, account=None, linktoken=None, amount=0.1 * 10**18):
+def fundLink(
+    contract_address, account=None, linktoken=None, amount=Web3.toWei(1, "ether")
+):
+    print(f"-->  Funding with LINK...")
     account = account if account else getAccount()
     linktoken = linktoken if linktoken else getContract("linkToken")
     tx_fund = linktoken.transfer(contract_address, amount, {"from": account})
     tx_fund.wait(1)
-    print(f"Contract has been funded with {Web3.fromWei(amount)} LINK!")
+    print(f"Contract has been funded with LINK!")
     return tx_fund
 
 
